76d886bfd7e5fc68151dc6295bd949102d0c47f5
Yoj <gotowhitesnow@gmail.com>
2024-10-06 18:31:08 +0400

    [types][reflection]
    type reflection based on compiler impl

--- lib/cmake/lib_utils.cmake
+++ lib/cmake/lib_utils.cmake
@@ -1,11 +1,11 @@
-function(declare_cpp_library project standart lib_sources)
+function(declare_cpp_library project standart)
 	
 	project (${project})
 
 	set(CMAKE_CXX_STANDARD ${standart})
 	set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
-	add_library(${project} ${lib_sources})
+	add_library(${project} ${ARGN})
 
 	target_include_directories(${project} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR})
 
--- lib/include/ym-sprite-editor/types.h
+++ lib/include/ym-sprite-editor/types.h
@@ -1,27 +1,51 @@
 #pragma once
 
-#define NTEST
-#include "reflect.h"
-
-namespace
-{
-	namespace reflect_lib = reflect;
-}
-
-namespace ym::types::internal
-{
-	template <typename T>
-	size_t compute_id()
-	{
-		return reflect_lib::type_id<T>();
-	}
-}
+#include <string>
 
 namespace ym::sprite_editor::types
 {
+	template <typename T> requires std::is_class_v<T>
+	std::string_view type_name()
+	{
+#ifdef _MSC_VER
+		std::string_view name = __FUNCSIG__;
+		constexpr std::string_view prefix = "type_name<";
+		constexpr std::string_view suffix = ">(void)";
+#elif defined(__clang__) || defined(__GNUC__)
+		std::string_view name = __PRETTY_FUNCTION__;
+		constexpr std::string_view prefix = "std::string_view type_name() [T = ";
+		constexpr std::string_view suffix = "]";
+#endif
+
+#ifdef _MSC_VER
+		if (auto prefix_size = name.find(prefix); prefix_size != std::string_view::npos)
+		{
+			name.remove_prefix(prefix_size + prefix.size());
+		}
+#else
+		name.remove_prefix(prefix.size());
+#endif
+
+		constexpr std::string_view class_prefix = "class ";
+		constexpr std::string_view struct_prefix = "struct ";
+
+		if (name.starts_with(class_prefix))
+		{
+			name.remove_prefix(class_prefix.size());
+		}
+		else if (name.starts_with(struct_prefix))
+		{
+			name.remove_prefix(struct_prefix.size());
+		}
+
+		name.remove_suffix(suffix.size());
+		return name;
+	}
+
 	template <typename T>
 	size_t type_id()
 	{
-		return ym::types::internal::compute_id<T>();
+		return std::hash<std::string_view>{}(type_name<T>());
 	}
 }
+
